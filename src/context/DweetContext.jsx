58b9c64156121b17dc2d1e745e99ae88372a2b30
Danny Seong <daehoon.seong@gmail.com>
2023-08-26 15:41:14 +1000

    Refactor: create http class - simplify function logic in dweet service - create class http containing fetching functionality.

--- src/context/DweetContext.jsx
+++ src/context/DweetContext.jsx
@@ -1,9 +1,11 @@
 import { createContext, useContext } from 'react';
 import DweetService from '../service/dweet';
+import { HttpClient } from '../network/http';
 
 export const DweetApiContext = createContext();
 
-const dweetApi = new DweetService(process.env.REACT_APP_BASE_URL);
+const http = new HttpClient(process.env.REACT_APP_BASE_URL);
+const dweetApi = new DweetService(http);
 
 export function DweetApiProvider({ children }) {
   return (
--- src/service/dweet.js
+++ src/service/dweet.js
@@ -1,25 +1,18 @@
 export default class DweetService {
-  constructor(baseURL) {
-    this.baseURL = baseURL;
+  constructor(http) {
+    this.http = http;
   }
 
   async getDweets(username) {
     const query = username ? `?username=${username}` : '';
-    const response = await fetch(`${this.baseURL}/dweets${query}`, {
+    return this.http.fetch(`/dweets${query}`, {
       method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
     });
-    const data = await response.json();
-    if (response.status !== 200) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 
   async postDweet(text) {
-    const response = await fetch(`${this.baseURL}/dweets`, {
+    return this.http.fetch(`/dweets`, {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         text,
         username: 'danny',
@@ -27,35 +20,20 @@ export default class DweetService {
         url: 'https://avatars.githubusercontent.com/u/99000722?v=4',
       }),
     });
-    const data = await response.json();
-    if (response.status !== 201) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 
   async deleteDweet(dweetId) {
-    const response = await fetch(`${this.baseURL}/dweets/${dweetId}`, {
+    return this.http.fetch(`/dweets/${dweetId}`, {
       method: 'DELETE',
-      headers: { 'Content-Type': 'application/json' },
     });
-    if (response.status !== 204) {
-      throw new Error('An unexpected error has occurred');
-    }
   }
 
   async updateDweet(dweetId, text) {
-    const response = await fetch(`${this.baseURL}/dweets/${dweetId}`, {
+    return this.http.fetch(`/dweets/${dweetId}`, {
       method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         text,
       }),
     });
-    const data = await response.json();
-    if (response.status !== 200) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 }
